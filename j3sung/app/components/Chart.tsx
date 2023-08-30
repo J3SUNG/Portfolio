8a0a7ae7c0bae580c58e4d300fe5c4a9c87cab62
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-30 23:26:20 +0900

    Fix: Skill 차트 수정

--- j3sung/app/components/Chart.tsx
+++ j3sung/app/components/Chart.tsx
@@ -21,24 +21,26 @@ const Chart = (props: propsType) => {
     "#525FE1 ",
   ];
   return (
-    <PieChart
-      data={[
-        {
-          value: ability * 10,
-          color: backColor[ability],
-          name: "name1",
-        },
-      ]}
-      className="w-[50px] h-[50px] sm:w-[150px] sm:h-[150px] m-[10px] sm:m-[20px] mt-[10px] sm:mt-[40px]"
-      reveal={ability * 10} //퍼센트 치수
-      lineWidth={14} //도넛 두께
-      background="#f3f3f3"
-      lengthAngle={360}
-      rounded
-      // animate
-      // animationDuration={1000}
-      labelPosition={0}
-    />
+    <div className="-rotate-90 ">
+      <PieChart
+        data={[
+          {
+            value: ability * 10,
+            color: backColor[ability],
+            name: "name1",
+          },
+        ]}
+        className="w-[50px] h-[50px] sm:w-[150px] sm:h-[150px] m-[10px] sm:m-[20px] mt-[10px] sm:mt-[40px]"
+        reveal={ability * 10} //퍼센트 치수
+        lineWidth={14} //도넛 두께
+        background="#f3f3f3"
+        lengthAngle={360}
+        rounded
+        // animate
+        // animationDuration={1000}
+        labelPosition={0}
+      />
+    </div>
   );
 };
 
