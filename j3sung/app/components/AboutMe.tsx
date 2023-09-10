d15045e72f9441220aac5077b254cf8e3c747009
wptjd6141@naver.com <wptjd6141@naver.com>
2023-09-10 23:57:34 +0900

    Feat: AboutMe 내용 추가

--- j3sung/app/components/AboutMe.tsx
+++ j3sung/app/components/AboutMe.tsx
@@ -13,23 +13,15 @@ export default function AboutMe() {
           <Image src={profileImg} alt="profile image" className="w-[50vw] sm:w-[360px] " />
         </div>
         <div className="absolute left-0 top-[338px] sm:top-[780px] bg-sky-100 -z-10 w-[42vw] sm:w-[34%] h-[300px] sm:h-[540px]"></div>
-        <div className="ml-[0px] sm:ml-16 mt-6 font-Binggrae font-bold">
-          <ul className="mt-[10px] sm:mt-0 leading-5 sm:leading-8 text-[8px] sm:text-[18px]">
-            <li>이름 : 이제성 (만 28세)</li>
-            <li>자격증 : SQLD, 전자기기기능사, ICDL Start, ITQ OA Master</li>
-            <li>
-              수상 : <br />
-              - SSAFY 공통 프로젝트 우수상(구미 2반 1등) <br />
-              - SSAFY 관통 프로젝트 최우수상(최우수상) <br />
-              - Health-related IoT Tracking Platform Challenge(Outstanding Award) <br />
-              - 2019년 학부생 논문 및 캡스톤디자인 경진대회(우수상) <br />
-              - 2019 ACM-ICPC 서울대회(Certificate of Achievement) <br />
-              - 디지털로 만드는 스마트 하우스 공모전(창의상) <br />
-              - 2019 대경권 프로그래밍 경진대회(장려상) <br />
-            </li>
-            <li>교육 : 삼성 청년 SW 아카데미 1학기 (2023.01.04 ~ 2023.05.26 (800시간))</li>
-            <li>경험 : Qualcomm Institute 학생 인턴 (2020-09-15 ~ 2021-06-30)</li>
-            <li>경력 : 인투시어 개발부 경력직 (2020.03.26 ~ 2020.08.14)</li>
+        <div className="ml-[0px] sm:ml-16 mt-6 font-Binggrae">
+          <h2 className="text-[40px] font-bold">고민하는 개발자, 이제성입니다.</h2>
+          <ul className="mt-[10px] sm:mt-[20px] leading-5 sm:leading-[60px] text-[8px] sm:text-[22px]">
+            <li>- 안녕하세요! 주니어 프론트엔드 개발자 이제성입니다.</li>
+            <li>- Clean Code와 테스트, 성능 개선에 대한 고민을 생활화 합니다.</li>
+            <li>- 꾸준하게 학습한 내용을 Github에 남기고, 기술 블로그를 운영합니다. </li>
+            <li>- 새로운 것을 배우고 적용하는 것을 즐깁니다.</li>
+            <li>- 프로젝트를 진행하면 주로 팀장을 맡으며, 책임감을 가지고 임합니다.</li>
+            <li>- 대화하는 것을 좋아하며, 협업에 관심이 많습니다.</li>
           </ul>
         </div>
       </div>
