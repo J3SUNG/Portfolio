a58579fffee8fba3715d8f3dddd5855eaf1b4f7e
J3SUNG <wptjd6141@naver.com>
2023-10-25 17:58:32 +0900

    Fix : 프로필 이미지 수정

--- j3sung/app/components/AboutMe.tsx
+++ j3sung/app/components/AboutMe.tsx
@@ -1,5 +1,4 @@
 import profileImg from "/public/img/profile.jpg";
-import wideProfile from "/public/img/wideProfile.jpg";
 import Image from "next/image";
 
 export default function AboutMe() {
@@ -11,11 +10,11 @@ export default function AboutMe() {
       <h2 className="text-4xl font-bold text-left text-blue-900 sm:text-7xl">About me</h2>
       <div className="sm:flex">
         <div className="flex">
-          <Image src={wideProfile} alt="profile image" className="w-[50vw] sm:w-auto h-[480px]" />
+          <Image src={profileImg} alt="profile image" className="w-[50vw] sm:w-auto h-[480px]" />
         </div>
         <div className="absolute left-0 top-[338px] sm:top-[780px] bg-sky-100 -z-10 w-[42vw] sm:w-[34%] h-[300px] sm:h-[540px]"></div>
         <div className="ml-[0px] sm:ml-10 mt-8 font-Binggrae">
-          <h2 className="text-[36px] font-bold">고민하는 프론트엔드 개발자, 이제성입니다.</h2>
+          <h3 className="text-[36px] font-bold">고민하는 프론트엔드 개발자, 이제성입니다.</h3>
           <ul className="mt-[10px] sm:mt-[20px] leading-5 sm:leading-[50px] text-[8px] sm:text-[22px]">
             <li>- 안녕하세요! 주니어 프론트엔드 개발자 이제성입니다.</li>
             <li>- "왜"라는 질문을 하고 "어떻게" 라는 방법을 찾습니다.</li>
