aeab0790108a4fb548f5fe9388d69540bcb1cd34
J3SUNG <wptjd6141@naver.com>
2023-11-23 11:13:05 +0900

    Feat : 포트폴리오 버전2 세팅

---
+++ portfolio_v2/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
