22ae2e0bb56a609aab72a6fdeae8e19d7b8498e8
J3SUNG <wptjd6141@naver.com>
2023-11-24 14:59:55 +0900

    Feat : 폰트 추가

--- portfolio_v2/app/components/Header.tsx
+++ portfolio_v2/app/components/Header.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="w-[1280px] h-[600px] m-auto bg-[#395fd3] rounded-[30px]">
+        <svg width={1280} height={600} viewBox="0 0 100 100">
+          <text
+            fontSize={40}
+            color="black"
+            stroke="black"
+            x={50}
+            y={50}
+            textAnchor="middle"
+            dominantBaseline="central"
+            className="font-pre-bold"
+          >
+            Front End
+          </text>
+        </svg>
+      </div>
+    </>
+  );
 };
 
 export default Header;
